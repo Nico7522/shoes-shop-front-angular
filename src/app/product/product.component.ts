1ae2a1f094c6455ab22db1c034990ee4782685cb
Nicolas <nico.daddabbo7100@gmail.com>
2024-01-30 14:27:17 +0100

    params

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { map } from 'rxjs';
 import { Product } from '../fake-data/interfaces/product-interface';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -10,11 +11,19 @@ import { Product } from '../fake-data/interfaces/product-interface';
 })
 export class ProductComponent implements OnInit {
   productList: Product[] = [];
-  constructor(private _productService: ProductService) {}
+  array: string[] = ['sqq', 'dqd'];
+  constructor(
+    private _productService: ProductService,
+    private _router: Router
+  ) {}
   ngOnInit(): void {
     this._productService
       .getProduct()
       // .pipe(map((p) => p.slice(1, 3)))
       .subscribe((p) => (this.productList = p));
+  }
+
+  next(): void {
+    this._router.navigate(['/'], { queryParams: { categories: this.array } });
   }
 }
