21f448cda5833c7e39fdcb9c08d087d815a65abe
Nicolas <nico.daddabbo7100@gmail.com>
2024-02-04 16:39:50 +0100

    ajout des pages de créations de produit pour test

--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductComponent } from './product.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { AddCategoriesComponent } from './add-categories/add-categories.component';
+import { AddSizesComponent } from './add-sizes/add-sizes.component';
 
-const routes: Routes = [{ path: '', component: ProductComponent }];
+const routes: Routes = [
+  { path: '', component: ProductComponent },
+  {
+    path: 'create',
+    component: CreateProductComponent,
+    children: [
+      { path: 'addcategories', component: AddCategoriesComponent },
+      { path: 'addsizes', component: AddSizesComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
+import { CreateProductComponent } from './create-product/create-product.component';
+import { AddCategoriesComponent } from './add-categories/add-categories.component';
+import { AddSizesComponent } from './add-sizes/add-sizes.component';
 
 
 @NgModule({
   declarations: [
-    ProductComponent
+    ProductComponent,
+    CreateProductComponent,
+    AddCategoriesComponent,
+    AddSizesComponent
   ],
   imports: [
     CommonModule,
